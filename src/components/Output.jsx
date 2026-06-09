8a783b443492503bc4ba042af9daa04303584629
root404 <mohammadrg360@gmail.com>
2026-06-09 20:48:05 +0330

    design ui Output component and define functions

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Box } from '@chakra-ui/react'
+import { useMemo } from 'react'
 
-const Output = () => {
-  return (
-    <div>Output</div>
-  )
+const Output = ({ html = "", css = "", javascript = "" }) => {
+    const srcDoc = useMemo(() => {
+        return `
+            <html>
+                <head>
+                    <style>${css}</style>
+                </head>
+                <body>
+                ${html}
+
+                <script>
+                    try{
+                        ${javascript}
+                    }catch(err){
+                        document.body.innerHtml = "<pre style='color:red'>" + err + "</pre>"
+                    }
+                </script>
+                </body>
+            </html>
+        `
+    }, [html, css, javascript])
+    return (
+        <Box>
+            <iframe
+                title='output'
+                srcDoc={srcDoc}
+                sandbox='allow-scripts'
+                style={{ width: "100%", height: "100%", border: "none", borderRadius: "10px", boxShadow: "0 0 20px green" }}
+            />
+        </Box>
+    )
 }
 
 export default Output
