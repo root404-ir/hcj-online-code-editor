2707a8650b824072cbd3db0b3546bb72c7e794a2
root404 <mohammadrg360@gmail.com>
2026-06-09 15:18:07 +0330

    create api for execute code

---
+++ src/components/api.js
@@ -0,0 +1,19 @@
+import { LANGUAGE_VERSION } from "../constants";
+import axios from "axios";
+
+const API = axios.create({
+    baseURL: "https://emkc.org/api/v2/piston"
+})
+
+export const executeCode = async (language, sourceCode) => {
+    const response = await API.post('/execute', {
+        "language": language,
+        "version": LANGUAGE_VERSION,
+        "files": [
+            {
+                "content": sourceCode
+            }
+        ]
+    })
+    return response.data
+}
