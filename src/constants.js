fe86810b9c684ab716885fb9123ba67ae761e4a0
root404 <mohammadrg360@gmail.com>
2026-06-09 15:12:58 +0330

    language version and code snippets

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const LANGUAGE_VERSION = {
+    javascript: "ES6+",
+    html: "5.2",
+    css: "3.0"
+}
+
+export const CODE_SNIPPETS = {
+    javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("me.mtjs.ir");\n`,
+    html: `\n<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">\n\t<title>Document</title>\n</head>\n<body>\n\t<h1>Hello, mtjs!</h1>\n</body>\n</html>\n`,
+    css: `\nbody {\n\tbackground-color: green;\n}\nh1 {\n\tcolor: white;\n\ttext-align: center;\n}\n`,
+}
