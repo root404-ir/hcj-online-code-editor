fe19d7a7ef71d3f79b5bdde8a2e13e6787f40d68
root404 <mohammadrg360@gmail.com>
2026-06-12 01:37:24 +0330

    style and set links to icons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,15 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Link } from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa";
 import { FaTelegram } from "react-icons/fa";
 import { FaEarthAsia } from "react-icons/fa6";
 
 const SocialIcons = () => {
-  const IconsSize = '1.5rem'
+  const IconsSize = '1.2rem'
   return (
     <Box display={'flex'} gap={6}>
-      <FaGithub size={IconsSize} />
-      <FaTelegram size={IconsSize} />
-      <FaEarthAsia size={IconsSize} />
+      <Link href="https://github.com/root404-ir" target="_blank" _focus={{ outline: "none" }} ><FaGithub size={IconsSize} /></Link>
+      <Link href="https://t.me/mtjsroot" target="_blank" _focus={{ outline: "none" }}><FaTelegram size={IconsSize} /></Link>
+      <Link href="http://me.mtjs.ir" target="_blank" _focus={{ outline: "none" }}><FaEarthAsia size={IconsSize} /></Link>
     </Box>
   )
 }
