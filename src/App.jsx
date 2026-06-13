5889cc69ea5c0cc436b95091f90d1ec770e31744
root404 <mohammadrg360@gmail.com>
2026-06-13 03:55:59 +0330

    Apply some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { useRef } from "react"
 import CodeEditor from "./components/CodeEditor"
 import { Box, Text } from "@chakra-ui/react"
 import SocialIcons from "./components/SocialIcons"
-
+import './style.css'
 
 const App = () => {
   const editorRef = useRef()
   return (
-    <Box bg={'black'} mt={4} padding={'0 10px'}>
-      <SocialIcons />
-      <Text fontFamily={'monospace'} className="text-9xl  text-center text-shadow-2xl text-shadow-green-500">online html,css,js code editor</Text>
-      <CodeEditor editorRef={editorRef} />
+    <Box>
+      <div className="code_container"></div>
+      <Box bg={'black'} mt={4} padding={'0 10px'}>
+        <Box>
+          <div className="flex justify-between">
+          <SocialIcons />
+            <Text fontFamily={'monospace'} className="text-9xl  text-center text-shadow-2xl text-shadow-green-500" fontSize={17}>online html,css,js code editor</Text>
+            <Text className="text-right" fontSize={17} fontFamily={'monospace'}>v0.2.0</Text>
+          </div>
+          <CodeEditor editorRef={editorRef} />
+        </Box>
+      </Box>
     </Box>
   )
 }
